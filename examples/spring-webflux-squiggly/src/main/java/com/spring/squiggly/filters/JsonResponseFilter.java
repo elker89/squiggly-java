6efe05e22936b105b3c84ecc611de9b03e5fd8c2
jarp <jonathanrpx@gmail.com>
2018-12-13 22:56:55 -0500

    feat(filter): Add AbstractSquigglyContextProvider & change ThreadLocal type

--- examples/spring-webflux-squiggly/src/main/java/com/spring/squiggly/filters/JsonResponseFilter.java
+++ examples/spring-webflux-squiggly/src/main/java/com/spring/squiggly/filters/JsonResponseFilter.java
@@ -1,20 +1,12 @@
 package com.spring.squiggly.filters;
 
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.bohnman.squiggly.Squiggly;
+import com.github.bohnman.squiggly.context.provider.AbstractSquigglyContextProvider;
 import com.github.bohnman.squiggly.util.SquigglyUtils;
-
-import java.nio.charset.StandardCharsets;
-import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.UnaryOperator;
-
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.http.server.reactive.ServerHttpResponse;
@@ -23,90 +15,117 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
-
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
 
 @Component
 @Slf4j
-@RequiredArgsConstructor
 @SuppressWarnings("unchecked")
 public final class JsonResponseFilter implements WebFilter {
 
-  @Value("${squiggly.json-response-filter.paramName:fields}")
-  private String queryParamName;
+    private static final ThreadLocal<String> FILTER_HOLDER = new ThreadLocal<>();
 
-  @NonNull
-  private ObjectMapper originalMapper;
+    private String queryParamName;
+    private ObjectMapper originalMapper;
 
-  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    return chain
-        .filter(Optional
-            .of(exchange.getRequest().getQueryParams())
-            .map(queryParams -> queryParams.getFirst(queryParamName))
-            .map(fields -> exchange
-                .mutate()
-                .response(new SquigglyServerResponseDecorator(exchange.getResponse(), fields, originalMapper))
-                .build())
-            .orElse(exchange));
-  }
-
-  private class SquigglyServerResponseDecorator extends ServerHttpResponseDecorator {
-
-    private ThreadLocal<ObjectMapper> context;
-
-    private SquigglyServerResponseDecorator(ServerHttpResponse source,
-                                            String fields,
-                                            ObjectMapper objectMapper) {
-      super(source);
-      context = ThreadLocal.withInitial(() -> Squiggly.init(objectMapper, fields));
-      log.trace("@@@ Applying json response filter [{}]", fields);
+    @Autowired
+    public JsonResponseFilter(
+            @Value("${squiggly.json-response-filter.paramName:fields}")
+                    String queryParamName,
+            ObjectMapper objectMapper) {
+        this.queryParamName = queryParamName;
+        this.originalMapper = Squiggly.init(objectMapper, new ContextProvider());
     }
 
-    @Override
-    public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
-      return body instanceof Flux ?
-          super.writeWith(((Flux) body)
-              .compose(dataBufferOperator())) :
-          super.writeWith(body);
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+        return chain
+                .filter(Optional
+                        .of(exchange.getRequest().getQueryParams())
+                        .map(queryParams -> queryParams.getFirst(queryParamName))
+                        .map(fields -> exchange
+                                .mutate()
+                                .response(new SquigglyServerResponseDecorator(
+                                        exchange.getResponse(), fields))
+                                .build())
+                        .orElse(exchange));
     }
 
-    private UnaryOperator<Flux<DataBuffer>> dataBufferOperator() {
-      return (source) -> source
-          .publishOn(Schedulers.parallel())
-          .map((buffer) -> bufferFactory()
-              .wrap(applySquigglyFilter(buffer)));
+    private class SquigglyServerResponseDecorator extends ServerHttpResponseDecorator {
+
+        private final String fields;
+
+        private SquigglyServerResponseDecorator(ServerHttpResponse source,
+                                                String fields) {
+            super(source);
+            this.fields = fields;
+            log.trace("@@@ Applying json response filter [{}]", fields);
+        }
+
+        @Override
+        public Mono<Void> writeWith(Publisher<? extends DataBuffer> body) {
+            return body instanceof Flux ?
+                    super.writeWith(((Flux) body)
+                            .compose(dataBufferOperator())) :
+                    super.writeWith(body);
+        }
+
+        private UnaryOperator<Flux<DataBuffer>> dataBufferOperator() {
+            return (source) -> source
+                    .publishOn(Schedulers.parallel())
+                    .map((buffer) -> bufferFactory()
+                            .wrap(applySquigglyFilter(buffer)));
+        }
+
+
+        private byte[] applySquigglyFilter(DataBuffer buffer) {
+            try {
+                FILTER_HOLDER.set(fields);
+                ObjectMapper mapper = originalMapper;
+                return SquigglyUtils
+                        .stringify(mapper, mapper
+                                .readValue(buffer.asInputStream(), Object.class))
+                        .getBytes(StandardCharsets.UTF_8);
+            } catch (Throwable ex) {
+                JsonResponseFilter.log.trace("@@@ Error applying json response filter...", ex);
+
+                byte[] original = new byte[buffer.readableByteCount()];
+                buffer.read(original);
+                return original;
+            } finally {
+                FILTER_HOLDER.remove();
+            }
+        }
+
+        @Override
+        public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
+            return body instanceof Flux ?
+                    super.writeAndFlushWith(((Flux) body)
+                            .flatMap(Function.identity())
+                            .compose(dataBufferOperator())
+                            .map(Flux::just)) :
+                    super.writeAndFlushWith(body);
+        }
     }
 
+    private class ContextProvider extends AbstractSquigglyContextProvider {
 
-    private byte[] applySquigglyFilter(DataBuffer buffer) {
-      try {
-        ObjectMapper mapper = context.get();
-        return SquigglyUtils
-            .stringify(mapper, mapper
-                .readValue(buffer.asInputStream(), new TypeReference<Object>() {
-                }))
-            .getBytes(StandardCharsets.UTF_8);
-      } catch (Throwable ex) {
-        JsonResponseFilter.log.trace("@@@ Error applying json response filter...", ex);
+        @Override
+        public boolean isFilteringEnabled() {
+            return Objects.nonNull(FILTER_HOLDER.get());
+        }
 
-        byte[] original = new byte[buffer.readableByteCount()];
-        buffer.read(original);
-        return original;
-      }
+        @Override
+        protected String getFilter(Class beanClass) {
+            return FILTER_HOLDER.get();
+        }
     }
-
-    @Override
-    public Mono<Void> writeAndFlushWith(Publisher<? extends Publisher<? extends DataBuffer>> body) {
-      return body instanceof Flux ?
-          super.writeAndFlushWith(((Flux) body)
-              .flatMap(Function.identity())
-              .compose(dataBufferOperator())
-              .map(Flux::just)) :
-          super.writeAndFlushWith(body);
-    }
-  }
 }
 
